08c3bd368b96d0b5b47b434b466a4bf388decd64
Muhammad-Uzair99 <uzairmaster97@gmail.com>
2023-09-09 23:22:47 +0500

    Refactored JS files to better utilize previous entries array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-let counter = document.getElementById("count")
-let entries = document.getElementById("entries")
+const counter = document.getElementById("count")
+const entries = document.getElementById("entries")
+const entriesArr = [];
+
 let count = 0;
-let add = () => (count++, counter.innerText = count);
-let subtract = () => (count--, counter.innerText = count);
-let save = () => (counter.innerText = 0, entries.textContent += `${count}, `, count = 0);
+const add = () => (count++, counter.innerText = count);
+const subtract = () => (count--, counter.innerText = count);
+const save = () => (counter.innerText = 0, entriesArr.push(count), entries.textContent = `Previous Entries: ${entriesArr.join(", ")}`, count = 0);
+
+const plusBtn = document.querySelector("#plus-btn");
+const minusBtn = document.querySelector("#minus-btn");
+const saveBtn = document.querySelector("#save-btn");
+
+plusBtn.addEventListener("click", () => add());
+minusBtn.addEventListener("click", () => subtract());
+saveBtn.addEventListener("click", () => save());
